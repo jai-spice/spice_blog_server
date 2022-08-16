43ebb0e0df97fab0dce61135ef523d28eac0441d
Jai Sachdeva <sachdeva.jai@gmail.com>
2022-08-16 09:47:26 +0530

    add CORS

--- bin/server.dart
+++ bin/server.dart
@@ -10,10 +10,26 @@ void main(List<String> args) async {
   // Use any available host or container IP (usually `0.0.0.0`).
   final ip = InternetAddress.anyIPv4;
 
+  const corsHeaders = {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'GET, POST, DELETE, OPTIONS',
+    'Access-Control-Allow-Headers': '*',
+  };
+
+  Response? _options(Request request) => (request.method == 'OPTIONS')
+      ? Response.ok(null, headers: corsHeaders)
+      : null;
+  Response _cors(Response response) => response.change(headers: corsHeaders);
+  final fixCORS =
+      createMiddleware(requestHandler: _options, responseHandler: _cors);
+
   await PostgreSQL.instance.open();
 
   // Configure a pipeline that logs requests.
-  final handler = Pipeline().addMiddleware(logRequests()).addHandler(router);
+  final handler = Pipeline()
+      .addMiddleware(fixCORS)
+      .addMiddleware(logRequests())
+      .addHandler(router);
 
   // For running in containers, we respect the PORT environment variable.
   final port = int.parse(Platform.environment['PORT'] ?? '8080');
